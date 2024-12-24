bc5f6fc90252dde62bac9708569c69def25c7dd3
AIDcGH <aviv.c000@gmail.com>
2024-12-24 20:20:48 +0200

    Added shrink ball along with random size change

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
 'use strict'
 
 function onBallClick(elBall, maxDiameter) {
-    var size = parseInt(elBall.style.width) || 100
-    size += getRandomInt(20, 61)
-    if (size >= maxDiameter) size = 100
+    changeSizeRandom(elBall, maxDiameter)
     
-    elBall.style.width = elBall.style.height = size + 'px'
-    elBall.innerText = size
-
     elBall.style.background = getRandomColor()
 }
 
+function changeSizeRandom(elBall, edgeCase, isReduce = false) {
+    var size = parseInt(elBall.style.width) || 100
+    size += getRandomInt(20,61) * (isReduce ? -1 : 1)
+    
+    if ((size >= edgeCase && !isReduce) || (size <= edgeCase && isReduce)) size = 100
+    
+    elBall.style.width = elBall.style.height = size + 'px'
+    elBall.innerText = size
+}
+
 function onBallSwap() {
-    var elBall1 = document.querySelector('.one')
-    var elBall2 = document.querySelector('.two')
+    const elBall1 = document.querySelector('.one')
+    const elBall2 = document.querySelector('.two')
 
     swapElStyle(elBall1, elBall2, 'background')
-    
+
     swapElStyle(elBall1, elBall2, 'width')
     swapElStyle(elBall1, elBall2, 'height')
-    
-    elBall1.innerText = parseInt(elBall1.style.width) 
+
+    elBall1.innerText = parseInt(elBall1.style.width)
     elBall2.innerText = parseInt(elBall2.style.width)
+}
+
+function onShrink() {
+    const elBall1 = document.querySelector('.one')
+    const elBall2 = document.querySelector('.two')
+
+    changeSizeRandom(elBall1, 100, true)
+    changeSizeRandom(elBall2, 100, true)
 }
