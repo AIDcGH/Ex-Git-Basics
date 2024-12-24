333eb4d8b242c79fd51563dd694679f01b04cbe8
AIDcGH <aviv.c000@gmail.com>
2024-12-24 17:40:54 +0200

    Ball size change randomized

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 function onBallClick(elBall) {
     var size = parseInt(elBall.style.width) || 100
-    size = size < 400 ? size + 50 : 100
+    size = size < 400 ? size + getRandomInt(20, 61) : 100
 
     elBall.style.width = elBall.style.height = size + 'px'
     elBall.innerText = size
