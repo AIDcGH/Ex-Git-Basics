e9d44c661f95789a1a85d416cc90fbe8e6491e0c
AIDcGH <aviv.c000@gmail.com>
2024-12-24 16:49:33 +0200

    Gave the ball size a limit that resets size

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 function onBallClick(elBall) {
     var size = parseInt(elBall.style.width) || 100
-    size  += 50
+    size = size < 400 ? size + 50 : 100
 
     elBall.style.width = elBall.style.height = size + 'px'
     elBall.innerText = size
