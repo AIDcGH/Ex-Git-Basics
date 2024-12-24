7fc164ffe184b0dbbba85701ae1c49e14142a833
AIDcGH <aviv.c000@gmail.com>
2024-12-24 18:38:24 +0200

    Made the size limit a variable

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict'
 
-function onBallClick(elBall) {
+function onBallClick(elBall, maxDiameter) {
     var size = parseInt(elBall.style.width) || 100
     size += getRandomInt(20, 61)
-    if (size >= 400) size = 100
+    if (size >= maxDiameter) size = 100
     
     elBall.style.width = elBall.style.height = size + 'px'
     elBall.innerText = size
