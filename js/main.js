e6a7a48c5b865ed8eb0b1bf012fcba8b56feffa1
AIDcGH <aviv.c000@gmail.com>
2024-12-24 17:53:15 +0200

    Added random color to ball and link to index

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@
 
 function onBallClick(elBall) {
     var size = parseInt(elBall.style.width) || 100
-    size = size < 400 ? size + getRandomInt(20, 61) : 100
-
+    size += getRandomInt(20, 61)
+    if (size >= 400) size = 100
+    
     elBall.style.width = elBall.style.height = size + 'px'
     elBall.innerText = size
+
+    elBall.style.background = getRandomColor()
 }
